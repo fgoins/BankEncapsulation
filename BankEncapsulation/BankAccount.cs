6ee695438abb190bee5eaba73bb1410d1d1cc38c
Faithtorian Goins <faithtoriangoins@gmail.com>
2023-02-14 16:01:15 -0600

    completed

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+
+namespace BankEncapsulation
+{
+    //A private field of type double named balance with a value of 0
+    //Define a method named Deposit that will accept a double and store that value in the balance field
+    //Define a method named GetBalance that will return the amount stored in the balance field
+    public class BankAccount
+    {
+        private double balance = 50;
+         public void Deposit(double amount) 
+        {
+            balance += amount;
+            Console.WriteLine($"{balance}");
+        }
+        public double GetBalance() 
+        {
+            return balance;
+        }
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+            Console.WriteLine("how much money are you depositing?");
+            var deposit = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("this is how much money is your account");
+            account.Deposit(deposit);
+            double final = account.GetBalance();
+            account.GetBalance();
         }
     }
 }
